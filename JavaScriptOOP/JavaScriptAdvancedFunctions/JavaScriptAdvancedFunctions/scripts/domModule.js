dac52fe2aa5387d50009bcd0db885c50265e2093
epitsin <epitsin@yahoo.com>
2014-06-26 10:56:44 +0300

    JavaScript OOP - Advanced Functions Homework

---
+++ JavaScriptOOP/JavaScriptAdvancedFunctions/JavaScriptAdvancedFunctions/scripts/domModule.js
@@ -0,0 +1,49 @@
+﻿var domModule = (function () {
+    var buffer = [];
+
+    function appendChild(element, parentSelector) {
+        var parent = document.querySelector(parentSelector);
+        parent.appendChild(element);
+    }
+
+    function removeChild(parentSelector, childSelector) {
+        var parent = document.querySelector(parentSelector);
+        var element = document.querySelector(childSelector);
+        parent.removeChild(element);
+    }
+
+    function addHandler(elementSelector, eventType, eventHandler) {
+        var element = document.querySelector(elementSelector);
+        element.addEventListener(eventType, eventHandler);
+    }
+
+    function appendToBuffer(parentSelector, element) {
+        if (buffer[parentSelector]) {
+            buffer[parentSelector].push(element);
+
+            if (buffer[parentSelector].length == 100) {
+                var frag = document.createDocumentFragment();
+                for (var i = 0; i < buffer[parentSelector].length; i++) {
+                    frag.appendChild(buffer[parentSelector][i]);
+                }
+
+                var parent = document.querySelector(parentSelector);
+                parent.appendChild(frag);
+
+                buffer[parentSelector] = [];
+            }
+
+        } else {
+            buffer[parentSelector] = [];
+            buffer[parentSelector].push(element);
+        }
+    }
+
+    return {
+        appendChild: appendChild,
+        removeChild: removeChild,
+        addHandler: addHandler,
+        appendToBuffer: appendToBuffer
+    };
+
+})();
---
+++ JavaScriptOOP/JavaScriptAdvancedFunctions/JavaScriptAdvancedFunctions/scripts/movingDivElements.js
@@ -0,0 +1,100 @@
+﻿var movingShapes = (function () {
+    function add(shape) {
+        var currDiv = document.createElement('div');
+
+        currDiv.innerHTML += '<strong>div</strong>';
+        currDiv.style.textAlign = 'center';
+
+        currDiv.style.width = getRandomNumber(20, 100) + 'px';
+        currDiv.style.height = getRandomNumber(20, 100) + 'px';
+
+        currDiv.style.backgroundColor = getRandomColor();
+        currDiv.style.color = getRandomColor();
+
+        currDiv.style.border = getRandomNumber(1, 20) + 'px' + ' solid ' + getRandomColor();
+        currDiv.style.borderRadius = getRandomNumber(0, 100) + 'px';
+
+        currDiv.style.position = 'absolute';
+
+        var wrapper = document.getElementById('wrapper');
+        wrapper.appendChild(currDiv);
+
+        if (shape == 'ellipse') {
+            var centerX = 200;
+            var centerY = 200;
+            var radius = 50;
+
+            var angle = 0;
+
+            var functionTimer = setInterval(function moveDivs() {
+                angle++;
+                if (angle === 360) {
+                    angle = 0;
+                }
+
+                var left = centerX + Math.cos((2 * 3.14 / 180) * (angle)) * radius;
+                var right = centerY + Math.sin((2 * 3.14 / 180) * (angle)) * radius;
+
+                currDiv.style.left = left + 'px';
+                currDiv.style.top = right + 'px';
+            }, 30);
+        }
+        else {
+            var centerX = 600;
+            var centerY = 200;
+            var direction = 0;
+
+            var functionTimer = setInterval(function moveDivs() {
+                if (direction > 3) {
+                    direction = 0;
+                }
+
+                switch (direction) {
+                    case 0: centerX += 5;
+                        if (centerX > 700) {
+                            direction++;
+                        }
+                        break;
+                    case 1: centerY += 5;
+                        if (centerY > 300) {
+                            direction++;
+                        }
+                        break;
+                    case 2: centerX -= 5;
+                        if (centerX < 600) {
+                            direction++;
+                        }
+                        break;
+                    case 3: centerY -= 5;
+                        if (centerY < 200) {
+                            direction++;
+                        }
+                        break;
+                }
+
+
+                currDiv.style.left = centerX + 'px';
+                currDiv.style.top = centerY + 'px';
+            }, 50);
+        }
+
+    }
+
+    function getRandomNumber(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+
+    function getRandomColor() {
+        var red = Math.floor(Math.random() * 256);
+        var green = Math.floor(Math.random() * 256);
+        var blue = Math.floor(Math.random() * 256);
+
+        return 'rgb(' + red + ',' + green + ',' + blue + ')';
+    }
+
+    return {
+        add: add
+    };
+
+})();
