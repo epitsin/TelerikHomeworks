49138c401d0e7a8a410759d5f541d4914cd9e705
epitsin <epitsin@yahoo.com>
2014-09-27 07:54:36 +0300

    Telerik Homeworks

---
+++ Data Structures and Algorithms/AdvancedDataStructures/PriorityQueueWithBinaryHeap/PriorityQueue.cs
@@ -0,0 +1,29 @@
+﻿namespace PriorityQueueWithBinaryHeap
+{
+    using System;
+
+    public class PriorityQueue<T> where T : IComparable<T>
+    {
+        private readonly BinaryHeap<T> heap;
+
+        public int Count
+        {
+            get { return this.heap.Count; }
+        }
+
+        public PriorityQueue()
+        {
+            this.heap = new BinaryHeap<T>();
+        }
+
+        public void Enqueue(T element)
+        {
+            this.heap.Insert(element);
+        }
+
+        public T Dequeue()
+        {
+            return this.heap.Remove();
+        }
+    }
+}
---
+++ Data Structures and Algorithms/AdvancedDataStructures/PriorityQueueWithBinaryHeap/PriorityQueueEntryPoint.cs
@@ -0,0 +1,28 @@
+﻿// 1.Implement a class PriorityQueue<T> based on the data structure "binary heap".
+
+namespace PriorityQueueWithBinaryHeap
+{
+    using System;
+
+    public class PriorityQueueEntryPoint
+    {
+        public static void Main()
+        {
+            var priorityQueueInts = new PriorityQueue<int>();
+
+            priorityQueueInts.Enqueue(1);
+            priorityQueueInts.Enqueue(10);
+            priorityQueueInts.Enqueue(2);
+            priorityQueueInts.Enqueue(1);
+            priorityQueueInts.Enqueue(3);
+            priorityQueueInts.Enqueue(13);
+            priorityQueueInts.Enqueue(4);
+            priorityQueueInts.Enqueue(5);
+
+            while (priorityQueueInts.Count > 0)
+            {
+                Console.WriteLine(priorityQueueInts.Dequeue());
+            }         
+        }
+    }
+}
